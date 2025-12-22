dd71541ea5c0596f4523ecdf90b99c44f1bcca3a
Morteza Aghaei <mi.piboy.org@gmail.com>
2025-12-22 23:30:11 +0330

    GitHub Pages Set

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <TaskProvider>
         <div className='mainSection'>
           {mobileOptimizedMode ?
